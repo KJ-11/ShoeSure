e3ab366e0dd0513f79f05ad6f41e14d4831ec2ba
KJ-11 <113939507+KJ-11@users.noreply.github.com>
2023-10-29 17:46:14 -0500

    Authenticity page frontend

--- app/src/main/java/com/cs407/shoesure/MainActivity.java
+++ app/src/main/java/com/cs407/shoesure/MainActivity.java
@@ -2,7 +2,9 @@ package com.cs407.shoesure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/cs407/shoesure/authenticityPage.java
@@ -0,0 +1,26 @@
+package com.cs407.shoesure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class authenticityPage extends AppCompatActivity {
+
+    public void backHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticity_page);
+    }
+
+
+}
