2caa1f217cc51ec300874652ede052da60cc6ecb
KJ-11 <113939507+KJ-11@users.noreply.github.com>
2023-11-11 21:16:46 -0600

    merged shoelist with main

--- app/src/main/java/com/cs407/shoesure/MainActivity.java
+++ app/src/main/java/com/cs407/shoesure/MainActivity.java
@@ -13,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void shoeList(View view) {
+        Intent intent = new Intent(this, ShoeListActivity.class);
+        startActivity(intent);
+    }
 }
