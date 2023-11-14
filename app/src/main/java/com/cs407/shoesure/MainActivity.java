c0ddac51b4ff7c353f08fce283cfc5c6b0008cf5
KJ-11 <113939507+KJ-11@users.noreply.github.com>
2023-11-13 18:37:22 -0600

    updating main activity

--- app/src/main/java/com/cs407/shoesure/MainActivity.java
+++ app/src/main/java/com/cs407/shoesure/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void shoeList(View view) {
         Intent intent = new Intent(this, ShoeListActivity.class);
         startActivity(intent);
