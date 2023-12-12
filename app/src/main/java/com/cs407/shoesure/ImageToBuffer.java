33a9c5b2db047964abec5efb3bbf6372757b3f2c
sreyassriv <sreyasssrivastava@gmail.com>
2023-12-11 23:05:30 -0600

    Image buffer check

--- app/src/main/java/com/cs407/shoesure/ImageToBuffer.java
+++ app/src/main/java/com/cs407/shoesure/ImageToBuffer.java
@@ -1,6 +1,7 @@
 package com.cs407.shoesure;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 
 import java.io.File;
 import java.nio.ByteBuffer;
@@ -19,13 +20,15 @@ public class ImageToBuffer {
                 File imgFile = new File(imagePath);
                 if (imgFile.exists()) {
                     bitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                    bitmap = preprocessImage(bitmap);
+                    bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-            // int imageSize = bitmap.getRowBytes() * bitmap.getHeight();
-            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(602112);
+            int imageSize = 224 * 224 * 4 * 3; //bitmap.getRowBytes() * bitmap.getHeight();
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(imageSize);
             byteBuffer.order(ByteOrder.nativeOrder());
 
             bitmap.copyPixelsToBuffer(byteBuffer);
@@ -35,5 +38,34 @@ public class ImageToBuffer {
         }
 
         return byteBufferList;
+    }
+
+    private Bitmap preprocessImage(Bitmap bitmap) {
+        // Apply rescale factor
+        bitmap = rescaleBitmap(bitmap, 1.0f / 255.0f);
+
+        // Apply shear and zoom transformations
+        bitmap = applyShearAndZoom(bitmap);
+
+        return bitmap;
+    }
+
+    private Bitmap rescaleBitmap(Bitmap bitmap, float scale) {
+        Matrix matrix = new Matrix();
+        matrix.postScale(scale, scale);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    private Bitmap applyShearAndZoom(Bitmap bitmap) {
+        // Apply shear transformation
+        Matrix matrix = new Matrix();
+        matrix.postRotate(30); // Rotate by 30 degrees (adjust as needed)
+        matrix.postScale(1.0f, 1.0f - 0.2f);
+        bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+
+        // Apply zoom transformation
+        matrix = new Matrix();
+        matrix.postScale(1.0f + 0.2f, 1.0f + 0.2f);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
     }
 }
